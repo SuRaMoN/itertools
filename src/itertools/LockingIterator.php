b2a03d418f844b1c54385540823d0e3d4b27f4fd
matthias <matthias@icontroller.be>
2013-02-15 13:15:44 +0100

    Added extra functionality to locking iterator

--- src/itertools/LockingIterator.php
+++ src/itertools/LockingIterator.php
@@ -7,38 +7,40 @@ use ArrayIterator;
 
 
 class LockingIterator extends IteratorIterator {
-	protected $lock;
+	protected $lockFp;
 	protected $dir;
+	protected $locknameMapper;
 
-	public function __construct($inner, $dir) {
+	public function __construct($inner, $dir, $locknameMapper = null) {
 		parent::__construct(is_array($inner) ? new ArrayIterator($inner) : $inner);
 		$this->dir = $dir;
+		$this->locknameMapper = $locknameMapper;
 	}
 
 	protected function lock($name) {
 		if(!is_dir($this->dir)) {
 			mkdir($this->dir, 0777, true);
 		}
-		$this->lock = fopen("{$this->dir}/$name", 'w+');
-		flock($this->lock, LOCK_EX);
+		$this->lockFp = fopen("{$this->dir}/$name", 'w+');
+		flock($this->lockFp, LOCK_EX);
 	}
 
 	protected function unlock() {
-		flock($this->lock, LOCK_UN);
-		fclose($this->lock);
-		$this->lock = null;
+		flock($this->lockFp, LOCK_UN);
+		fclose($this->lockFp);
+		$this->lockFp = null;
 	}
 
 	public function current() {
 		$current = parent::current();
-		if($this->lock === null) {
-			$this->lock($current);
+		if($this->lockFp === null) {
+			$this->lock(is_null($this->locknameMapper) ? $current : call_user_func($this->locknameMapper, $current));
 		}
 		return $current;
 	}
 
 	public function next() {
-		if($this->lock !== null) {
+		if($this->lockFp !== null) {
 			$this->unlock();
 		}
 		parent::next();
