c880ff1a95aa9a6fe72d08b538c1faae9d1728c2
matthias <matthias@icontroller.be>
2013-08-05 10:03:30 +0200

    Removed chunkediterator code duplication

--- src/itertools/ChunkedIterator.php
+++ src/itertools/ChunkedIterator.php
@@ -7,46 +7,10 @@ use Traversable;
 use ArrayIterator;
 
 
+/**
+ * Depricated: use ChunkingIterator
+ */
 class ChunkedIterator extends IteratorIterator
 {
-    protected $chunkSize;
-    protected $chunk;
-
-    public function __construct($iterator, $chunkSize) {
-        parent::__construct(is_array($iterator) ? new ArrayIterator($iterator) : $iterator);
-        $this->chunkSize = $chunkSize;
-		$this->chunk = array();
-    }
-
-    public function rewind() {
-		$this->getInnerIterator()->rewind();
-		$this->chunk = array();
-    }
-
-	private function ensureBatchPresent() {
-		if(!is_null(key($this->chunk))) {
-			// chunk not completely fetched;
-			return;
-		}
-        $inner = $this->getInnerIterator();
-        for ($i = 0; $i < $this->chunkSize && $inner->valid(); $i++) {
-            $this->chunk[] = $inner->current();
-            $inner->next();
-        }
-	}
-
-    public function next() {
-        $this->chunk = array();
-    }
-
-    public function current() {
-		$this->ensureBatchPresent();
-        return $this->chunk;
-    }
-
-    public function valid() {
-		$this->ensureBatchPresent();
-        return !empty($this->chunk);
-    }
 }
 
