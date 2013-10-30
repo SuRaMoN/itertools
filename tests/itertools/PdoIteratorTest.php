a002973fc95aebfd032b949877ca8c12614e8e44
matthias <matthias@icontroller.be>
2013-10-30 13:37:44 +0100

    Added tests for other fetch strategy for PdoIterator

--- tests/itertools/PdoIteratorTest.php
+++ tests/itertools/PdoIteratorTest.php
@@ -9,8 +9,9 @@ use stdClass;
 
 class PdoIteratorTest extends PHPUnit_Framework_TestCase
 {
-	/** @test */
-	public function testMainFunctionality()
+	protected $pdo;
+
+	public function setup()
 	{
 		$pdo = new PDO('sqlite::memory:');
 		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -24,8 +25,18 @@ class PdoIteratorTest extends PHPUnit_Framework_TestCase
 		foreach(range(0, 9) as $i) {
 			$pdo->exec('INSERT INTO entries ("name") VALUES (' . $i . ')');
 		}
+		$this->pdo = $pdo;
+	}
 
-		$it = new PdoIterator($pdo, 'SELECT * FROM entries');
+	public function tearDown()
+	{
+		$this->pdo = null;
+	}
+
+	/** @test */
+	public function testBasicFunctionality()
+	{
+		$it = new PdoIterator($this->pdo, 'SELECT * FROM entries');
 		$rows = iterator_to_array($it);
 		$this->assertEquals(10, count($rows));
 		$this->assertTrue($rows[0] instanceof stdClass);
@@ -33,8 +44,19 @@ class PdoIteratorTest extends PHPUnit_Framework_TestCase
 			$this->assertEquals($i, $row->name);
 		}
 
-		$it = new PdoIterator($pdo, 'SELECT * FROM entries WHERE name > :minName', array(':minName' => 5));
+		$it = new PdoIterator($this->pdo, 'SELECT * FROM entries WHERE name > :minName', array(':minName' => 5));
 		$this->assertEquals(4, count(iterator_to_array($it)));
+	}
+
+	/** @test */
+	public function testFetchAssoc()
+	{
+		$it = new PdoIterator($this->pdo, 'SELECT * FROM entries', array(), PDO::FETCH_ASSOC);
+		$rows = iterator_to_array($it);
+		$this->assertEquals(10, count($rows));
+		foreach($rows as $i => $row) {
+			$this->assertEquals($i, $row['name']);
+		}
 	}
 }
 
