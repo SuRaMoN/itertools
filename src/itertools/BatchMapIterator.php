55e3602e5717924ea563e9a8dd28b345b84633aa
matthias <matthias@icontroller.be>
2014-05-24 02:44:51 +0200

    MIN [ALL] Major enhancements to BatchMapIterator

--- src/itertools/BatchMapIterator.php
+++ src/itertools/BatchMapIterator.php
@@ -2,14 +2,132 @@
 
 namespace itertools;
 
+use InvalidArgumentException;
+use IteratorIterator;
 
-class BatchMapIterator extends ChainIterator
+/**
+ * Maps iterator values using a callback function but in batches.
+ */
+class BatchMapIterator extends IteratorIterator
 {
-	public function __construct($innerItererator, $callback, $batchSize = 100)
+	const DONT_SPLIT_KEY_VALUES = 0;
+	const SPLIT_KEY_VALUES = 1;
+
+	const DONT_ADD_KEY_LIST = 0;
+	const ADD_KEY_LIST = 2;
+
+	const ALLOW_MERGE = 0;
+	const DONT_ALLOW_MERGE = 4;
+
+	protected $mapCallbacks;
+	protected $type;
+	protected $batchSize;
+    protected $batch;
+    protected $batchKeys;
+
+	public function __construct($innerItererator, $mapCallback, $batchSize = 100, $type = 0)
 	{
-		$batches = new ChunkingIterator($innerItererator, $batchSize);
-		$mappedBatches = new MapIterator($batches, $callback);
-		return parent::__construct($mappedBatches, self::DONT_USE_KEYS);
+		if (! is_callable($mapCallback)) {
+			throw new InvalidArgumentException('The callback must be callable');
+		}
+		if(($type & self::DONT_ALLOW_MERGE) == 0 && $innerItererator instanceof self && $batchSize == $innerItererator->getBatchSize() && $type == $innerItererator->getType()) {
+			parent::__construct(IterUtil::asTraversable($innerItererator->getInnerIterator()));
+			$this->mapCallbacks = array_merge($innerItererator->getMapCallbacks(), array($mapCallback));
+		} else {
+			parent::__construct(IterUtil::asTraversable($innerItererator));
+			$this->mapCallbacks = array($mapCallback);
+		}
+		$this->type = $type;
+		$this->batchSize = $batchSize;
 	}
+
+	public function getType()
+	{
+		return $this->type;
+	}
+
+	public function getMapCallbacks()
+	{
+		return $this->mapCallbacks;
+	}
+
+	public function getBatchSize()
+	{
+		return $this->batchSize;
+	}
+
+	protected function ensureBatchPresent()
+	{
+		$batch = array();
+		$batchKeys = array();
+        $inner = $this->getInnerIterator();
+		if(self::SPLIT_KEY_VALUES & $this->type) {
+			for($i = 0; $i < $this->batchSize && $inner->valid(); $i++, $inner->next()) {
+				$batch[] = array($inner->key(), $inner->current());
+			}
+		} else {
+			for($i = 0; $i < $this->batchSize && $inner->valid(); $i++, $inner->next()) {
+				$batch[] = $inner->current();
+				$batchKeys[] = $inner->key();
+			}
+		}
+
+		if((self::ADD_KEY_LIST & $this->type) != 0) {
+			foreach($this->mapCallbacks as $mapCallback) {
+				list($batchKeys, $batch) = call_user_func($mapCallback, $batchKeys, $batch);
+			}
+		} else {
+			foreach($this->mapCallbacks as $mapCallback) {
+				$batch = call_user_func($mapCallback, $batch);
+			}
+		}
+
+		$this->batch = $batch;
+		reset($this->batch);
+
+		$this->batchKeys = $batchKeys;
+		reset($this->batchKeys);
+	}
+
+    public function rewind()
+	{
+		$this->getInnerIterator()->rewind();
+		$this->batch = array();
+		$this->batchKeys = array();
+    }
+
+    public function valid()
+	{
+		if(null === key($this->batch)) {
+			$this->ensureBatchPresent();
+		}
+        return null !== key($this->batch);
+    }
+
+    public function next()
+	{
+		next($this->batch);
+		next($this->batchKeys);
+    }
+
+    public function key()
+	{
+		if(self::SPLIT_KEY_VALUES & $this->type) {
+			$result = current($this->batch);
+			return $result[0];
+		} else {
+			return current($this->batchKeys);
+		}
+    }
+
+    public function current()
+	{
+		if(self::SPLIT_KEY_VALUES & $this->type) {
+			$result = current($this->batch);
+			return $result[1];
+		} else {
+			return current($this->batch);
+		}
+    }
 }
  
--- tests/itertools/BatchMapIteratorTest.php
+++ tests/itertools/BatchMapIteratorTest.php
@@ -2,6 +2,7 @@
 
 namespace itertools;
 
+use ArrayIterator;
 use PHPUnit_Framework_TestCase;
 
 
@@ -18,6 +19,44 @@ class BatchMapIteratorTest extends PHPUnit_Framework_TestCase
 			return $elements;
 		}, 3);
 		$this->assertEquals(array(4, 4, 4, 4, 4, 4, 2), iterator_to_array($mappedInput));
+	}
+
+	/** @test */
+	public function testBatchMapIteratorMerge()
+	{
+		$input = new ArrayIterator(array(1, 1, 1, 1));
+		$mappedInput = new BatchMapIterator(new BatchMapIterator($input, function($batch) {
+			foreach($batch as & $a) { $a += 1; }
+			return $batch;
+		}, 3), function($batch) {
+			foreach($batch as & $a) { $a *= 2; }
+			return $batch;
+		}, 3);
+		$this->assertEquals(array(4, 4, 4, 4), iterator_to_array($mappedInput));
+		$this->assertSame($input, $mappedInput->getInnerIterator());
+	}
+
+	/** @test */
+	public function testSplitKeyValues()
+	{
+		$input = array(3 => 7, 4 => 8, 7 => 2);
+		$mappedInput = new BatchMapIterator($input, function($pairs) {
+			foreach($pairs as & $pair) {
+				$pair = array($pair[1], $pair[0]);
+			}
+			return $pairs;
+		}, 3, BatchMapIterator::SPLIT_KEY_VALUES);
+		$this->assertEquals(array(7 => 3, 8 => 4, 2 => 7), iterator_to_array($mappedInput));
+	}
+
+	/** @test */
+	public function testAddKeyList()
+	{
+		$input = array(3 => 7, 4 => 8, 7 => 2);
+		$mappedInput = new BatchMapIterator($input, function($keys, $values) {
+			return array($values, $keys);
+		}, 3, BatchMapIterator::ADD_KEY_LIST);
+		$this->assertEquals(array(7 => 3, 8 => 4, 2 => 7), iterator_to_array($mappedInput));
 	}
 }
 
