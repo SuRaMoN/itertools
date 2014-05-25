009066fee4764cb73bbd67b471bdb11e5025ca37
matthias <matthias@icontroller.be>
2014-05-25 16:41:05 +0200

    Added invocationlimit to callbackiterator

--- src/itertools/CallbackIterator.php
+++ src/itertools/CallbackIterator.php
@@ -3,21 +3,39 @@
 namespace itertools;
 
 
-class CallbackIterator extends CurrentCachedIterator {
-
+class CallbackIterator extends CurrentCachedIterator
+{
+	protected $maxInvocations;
 	protected $callback;
+	protected $invocationCount;
 
-	public function __construct($callback) {
+	public function __construct($callback, $maxInvocations = INF)
+	{
+		$this->maxInvocations = $maxInvocations;
 		parent::__construct(new RepeatIterator(true));
 		$this->callback = $callback;
 	}
 
-	public function uncachedCurrent() {
+	public function rewind()
+	{
+		$this->invocationCount = 0;
+		parent::rewind();
+	}
+
+	public function next()
+	{
+		$this->invocationCount += 1;
+		parent::next();
+	}
+
+	public function uncachedCurrent()
+	{
         return call_user_func($this->callback);
     }
 
-	public function valid() {
-		return true;
+	public function valid()
+	{
+		return $this->invocationCount < $this->maxInvocations;
 	}
 }
 
--- tests/itertools/CallbackIteratorTest.php
+++ tests/itertools/CallbackIteratorTest.php
@@ -5,10 +5,11 @@ namespace itertools;
 use PHPUnit_Framework_TestCase;
 
 
-class CallbackIteratorTest extends PHPUnit_Framework_TestCase {
-
+class CallbackIteratorTest extends PHPUnit_Framework_TestCase
+{
 	/** @test */
-	public function testMainFunctionality() {
+	public function testMainFunctionality()
+	{
 		$count = 0;
 		$it = new CallbackIterator(function() use(&$count) { $count += 1; });
 
@@ -25,6 +26,13 @@ class CallbackIteratorTest extends PHPUnit_Framework_TestCase {
 		$it->current();
 
 		$this->assertEquals(3, $count);
+	}
+
+	/** @test */
+	public function testMaxInvocations()
+	{
+		$it = new CallbackIterator(function() { return 1; }, 3);
+		$this->assertCount(3, iterator_to_array($it));
 	}
 }
 
