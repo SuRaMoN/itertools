9da1680dd17b9994b9a631136a83a23c0897225e
matthias <matthias@icontroller.be>
2015-07-28 15:36:38 +0200

    Added break functionality to callbackiterator

--- src/itertools/CallbackIterator.php
+++ src/itertools/CallbackIterator.php
@@ -5,37 +5,44 @@ namespace itertools;
 
 class CallbackIterator extends CurrentCachedIterator
 {
-	protected $maxInvocations;
-	protected $callback;
-	protected $invocationCount;
+    protected $maxInvocations;
+    protected $callback;
+    protected $breakCallback;
+    protected $invocationCount;
+    protected $breakCalled;
 
-	public function __construct($callback, $maxInvocations = INF)
-	{
-		$this->maxInvocations = $maxInvocations;
-		parent::__construct(new RepeatIterator(true));
-		$this->callback = $callback;
-	}
-
-	public function rewind()
-	{
-		$this->invocationCount = 0;
-		parent::rewind();
-	}
-
-	public function next()
-	{
-		$this->invocationCount += 1;
-		parent::next();
-	}
-
-	public function uncachedCurrent()
-	{
-        return call_user_func($this->callback);
+    public function __construct($callback, $maxInvocations = INF)
+    {
+        parent::__construct(new RepeatIterator(true));
+        $this->callback = $callback;
+        $this->maxInvocations = $maxInvocations;
+        $breakCalled = &$this->breakCalled;
+        $this->breakCallback = function() use (&$breakCalled) {
+            $breakCalled = true;
+        };
     }
 
-	public function valid()
-	{
-		return $this->invocationCount < $this->maxInvocations;
-	}
+    public function rewind()
+    {
+        $this->breakCalled = false;
+        $this->invocationCount = 0;
+        parent::rewind();
+    }
+
+    public function next()
+    {
+        $this->invocationCount += 1;
+        parent::next();
+    }
+
+    public function uncachedCurrent()
+    {
+        return call_user_func($this->callback, $this->breakCallback);
+    }
+
+    public function valid()
+    {
+        return $this->invocationCount < $this->maxInvocations && ! $this->breakCalled;
+    }
 }
 
