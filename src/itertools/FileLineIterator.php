a86a3d8f4896080434a84f506dd2e85f73fa8ea6
matthias <matthias@icontroller.be>
2017-01-09 16:15:11 +0100

    Added guzzle support to filelineiterator

--- src/itertools/FileLineIterator.php
+++ src/itertools/FileLineIterator.php
@@ -9,60 +9,65 @@ use InvalidArgumentException;
 
 class FileLineIterator extends TakeWhileIterator
 {
-	protected $fileHandle;
-	protected $closeFileHandleOnDestruct;
+    protected $fileHandle;
+    protected $closeFileHandleOnDestruct;
+    protected $guzzleStream;
 
-	public function __construct($file, $options = array())
-	{
-		$defaultOptions = array(
-			'includeWhitespace' => false,
-			'fromEncoding' => null,
-		);
+    public function __construct($file, $options = array())
+    {
+        $defaultOptions = array(
+            'includeWhitespace' => false,
+            'fromEncoding' => null,
+        );
 
-		$unknownOptions = array_diff(array_keys($options), array_keys($defaultOptions));
-		if(count($unknownOptions) != 0) {
-			throw new InvalidArgumentException('Unknown options specified: ' . implode(', ', $unknownOptions));
-		}
+        $unknownOptions = array_diff(array_keys($options), array_keys($defaultOptions));
+        if (count($unknownOptions) != 0) {
+            throw new InvalidArgumentException('Unknown options specified: ' . implode(', ', $unknownOptions));
+        }
 
-		$options = (object) array_merge($defaultOptions, $options);
+        $options = (object) array_merge($defaultOptions, $options);
 
-		if(is_resource($file)) {
-			$this->fileHandle = $file;
-			$this->closeFileHandleOnDestruct = false;
-			if(null !== $options->fromEncoding) {
-				throw new Exception('Source encoding can only be specified if constructed with file path');
-			}
-		} else if(is_string($file)) {
-			$this->fileHandle = @fopen($file, 'r');
-			if($this->fileHandle === false) {
-				throw new InvalidArgumentException("Could not open file with path: '$file'");
-			}
-			if(null !== $options->fromEncoding) {
-				stream_filter_append($this->fileHandle, 'convert.iconv.' . $options->fromEncoding . '/UTF-8');
-			}
-			$this->closeFileHandleOnDestruct = true;
+        if ($file instanceof \Guzzle\Stream\StreamInterface) {
+            $this->guzzleStream = $file;
+            $file = $file->getStream();
+        }
 
-		} else {
-			throw new InvalidArgumentException('You must provide either a stream or filename to the file line iterator, you provided a ' . gettype($file));
-		}
+        if (is_resource($file)) {
+            $this->fileHandle = $file;
+            $this->closeFileHandleOnDestruct = false;
+            if(null !== $options->fromEncoding) {
+                throw new Exception('Source encoding can only be specified if constructed with file path');
+            }
+        } else if (is_string($file)) {
+            $this->fileHandle = @fopen($file, 'r');
+            if ($this->fileHandle === false) {
+                throw new InvalidArgumentException("Could not open file with path: '$file'");
+            }
+            if (null !== $options->fromEncoding) {
+                stream_filter_append($this->fileHandle, 'convert.iconv.' . $options->fromEncoding . '/UTF-8');
+            }
+            $this->closeFileHandleOnDestruct = true;
 
-		$fileHandle = $this->fileHandle;
-		$lineIterator = new CallbackIterator(function() use ($fileHandle, $options) {
-			$line = fgets($fileHandle);
-			if($line !== false && !$options->includeWhitespace) {
-				$line = rtrim($line, "\r\n");
-			}
-			return $line;
-		});
-		parent::__construct($lineIterator, function($line) { return $line !== false; });
-	}
+        } else {
+            throw new InvalidArgumentException('You must provide either a stream or filename to the file line iterator, you provided a ' . gettype($file));
+        }
 
-	public function __destruct()
-	{
-		if($this->fileHandle !== null && $this->closeFileHandleOnDestruct) {
-			fclose($this->fileHandle);
-			$this->fileHandle = null;
-		}
-	}
+        $fileHandle = $this->fileHandle;
+        $lineIterator = new CallbackIterator(function () use ($fileHandle, $options) {
+            $line = fgets($fileHandle);
+            if ($line !== false && !$options->includeWhitespace) {
+                $line = rtrim($line, "\r\n");
+            }
+            return $line;
+        });
+        parent::__construct($lineIterator, function ($line) { return $line !== false; });
+    }
+
+    public function __destruct()
+    {
+        if ($this->fileHandle !== null && $this->closeFileHandleOnDestruct) {
+            fclose($this->fileHandle);
+            $this->fileHandle = null;
+        }
+    }
 }
-
