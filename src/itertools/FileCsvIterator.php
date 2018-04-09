39009f1390ad42d807660f2986ec3645e3cb4de8
David Suys <suysdavid@gmail.com>
2018-04-09 14:35:06 +0200

    Add GuzzleHttp support (#9)
    * Add GuzzleHttp support
    
    * Add phpunit and guzzlehttp dependency

--- src/itertools/FileCsvIterator.php
+++ src/itertools/FileCsvIterator.php
@@ -6,63 +6,67 @@ use Exception;
 use InvalidArgumentException;
 use SplFileInfo;
 
-
 class FileCsvIterator extends AbstractCsvIterator
 {
-	protected $fileHandle;
-	protected $closeFileHandleOnDestruct;
-	protected $guzzleStream;
+    protected $fileHandle;
+    protected $closeFileHandleOnDestruct;
+    protected $guzzleStream;
 
-	public function __construct($file, array $options = array())
-	{
-		$defaultOptions = array(
-			'length' => 0,
-			'fromEncoding' => null,
-		);
-		$this->options = array_merge($defaultOptions, $options);
+    public function __construct($file, array $options = array())
+    {
+        $defaultOptions = array(
+            'length' => 0,
+            'fromEncoding' => null,
+        );
+        $this->options = array_merge($defaultOptions, $options);
 
-		if($file instanceof SplFileInfo) {
-			$file = $file->getPathname();
-		}
+        if ($file instanceof SplFileInfo) {
+            $file = $file->getPathname();
+        }
 
-		if($file instanceof \Guzzle\Stream\StreamInterface) {
-			$this->guzzleStream = $file;
-			$file = $file->getStream();
-		}
+        if ($file instanceof \GuzzleHttp\Stream\StreamInterface) {
+            $this->guzzleStream = $file;
+            $file = (clone $file)->detach();
+        }
 
-		if(is_resource($file)) {
-			$this->fileHandle = $file;
-			$this->closeFileHandleOnDestruct = false;
-			if(null !== $this->options['fromEncoding']) {
-				throw new Exception('Source encoding can only be specified if constructed with file path');
-			}
-		} else if(is_string($file)) {
-			$this->fileHandle = @fopen($file, 'r');
-			if($this->fileHandle === false) {
-				throw new InvalidArgumentException("Could not open csv file with path: '$file'");
-			}
-			if(null !== $this->options['fromEncoding']) {
-				stream_filter_append($this->fileHandle, 'convert.iconv.' . $this->options['fromEncoding'] . '/UTF-8');
-			}
-			$this->closeFileHandleOnDestruct = true;
-		} else {
-			throw new InvalidArgumentException('You must provide either a stream or filename to the csv iterator, you provided a ' . gettype($file));
-		}
+        if ($file instanceof \Guzzle\Stream\StreamInterface) {
+            $this->guzzleStream = $file;
+            $file = $file->getStream();
+        }
 
-		parent::__construct(array_diff_key($options, $defaultOptions));
-	}
+        if (is_resource($file)) {
+            $this->fileHandle = $file;
+            $this->closeFileHandleOnDestruct = false;
+            if (null !== $this->options['fromEncoding']) {
+                throw new Exception('Source encoding can only be specified if constructed with file path');
+            }
+        } elseif (is_string($file)) {
+            $this->fileHandle = @fopen($file, 'r');
+            if ($this->fileHandle === false) {
+                throw new InvalidArgumentException("Could not open csv file with path: '$file'");
+            }
+            if (null !== $this->options['fromEncoding']) {
+                stream_filter_append($this->fileHandle, 'convert.iconv.' . $this->options['fromEncoding'] . '/UTF-8');
+            }
+            $this->closeFileHandleOnDestruct = true;
+        } else {
+            throw new InvalidArgumentException('You must provide either a stream or filename to the csv iterator, you provided a ' . gettype($file));
+        }
 
-	public function retrieveNextCsvRow()
-	{
-		return fgetcsv($this->fileHandle, $this->options['length'], $this->options['delimiter'], $this->options['enclosure'], $this->options['escape']);
-	}
+        parent::__construct(array_diff_key($options, $defaultOptions));
+    }
 
-	public function __destruct()
-	{
-		if(null !== $this->fileHandle && $this->closeFileHandleOnDestruct) {
-			fclose($this->fileHandle);
-			$this->fileHandle = null;
-		}
-	}
+    public function retrieveNextCsvRow()
+    {
+        return fgetcsv($this->fileHandle, $this->options['length'], $this->options['delimiter'], $this->options['enclosure'], $this->options['escape']);
+    }
+
+    public function __destruct()
+    {
+        if (null !== $this->fileHandle && $this->closeFileHandleOnDestruct) {
+            fclose($this->fileHandle);
+            $this->fileHandle = null;
+        }
+    }
 }
 
